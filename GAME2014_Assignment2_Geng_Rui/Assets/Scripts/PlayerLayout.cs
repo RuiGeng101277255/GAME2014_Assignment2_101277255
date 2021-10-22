230b5fe732257682d10713b4d632f88502829ac5
RuiGeng <ruichen.gengli@georgebrown.ca>
2021-10-22 02:39:01 -0400

    connected bottons to the placeable grids

--- GAME2014_Assignment2_Geng_Rui/Assets/Scripts/PlayerLayout.cs
+++ GAME2014_Assignment2_Geng_Rui/Assets/Scripts/PlayerLayout.cs
@@ -6,10 +6,12 @@ public class PlayerLayout : MonoBehaviour
 {
     public GameObject PlaceableGrids;
 
+    private SpriteRenderer spriteR;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,9 +24,16 @@ public class PlayerLayout : MonoBehaviour
     {
         //spawn player
 
-
+        spriteR.color = Color.black;
 
         //...
         //PlaceableGrids.SetActive(false);
+    }
+
+    public void PlacePlayer()
+    {
+        Debug.Log("Clicked");
+        spriteR.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        Destroy(this);
     }
 }

398d633955e5637094f60360584e2fe134cef435
RuiGeng <ruichen.gengli@georgebrown.ca>
2021-10-23 18:42:41 -0400

    minor mod to the prefabs

--- GAME2014_Assignment2_Geng_Rui/Assets/Scripts/PlayerLayout.cs
+++ GAME2014_Assignment2_Geng_Rui/Assets/Scripts/PlayerLayout.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class PlayerLayout : MonoBehaviour
 {
     public GameObject PlaceableGrids;
+    public Vector2 RowColNum;
+    public PlayerAttackScript Scythe;
+    public PlayerAttackScript Hammer;
+    public PlayerAttackScript Rifle;
+    public PlayerAttackScript Bomb;
 
     private SpriteRenderer spriteR;
+    private AttackTypes setType;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteR = GetComponent<SpriteRenderer>();
+        setType = AttackTypes.NONE;
     }
 
     // Update is called once per frame
@@ -25,6 +32,7 @@ public class PlayerLayout : MonoBehaviour
         //spawn player
 
         spriteR.color = Color.black;
+        setType = type;
 
         //...
         //PlaceableGrids.SetActive(false);
@@ -34,6 +42,20 @@ public class PlayerLayout : MonoBehaviour
     {
         Debug.Log("Clicked");
         spriteR.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        
+        if ((setType != AttackTypes.NONE) && (setType != null))
+        {
+            switch (setType)
+            {
+                case AttackTypes.SCYTHE:
+                    var tempNewPlayer = MonoBehaviour.Instantiate(Scythe);
+                    //set orientation and active
+                    break;
+
+            }
+        }
+        
+
         Destroy(this);
     }
 }
